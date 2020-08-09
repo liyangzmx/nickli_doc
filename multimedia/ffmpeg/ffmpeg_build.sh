0b0ecb2b216d8f30dc8debbb03af6d59844e72bf
Li Yang <liyangzmx@163.com>
2020-08-09 13:45:20 +0800

    Add fdk_aac support and fix any error.

--- multimedia/ffmpeg/ffmpeg_build.sh
+++ multimedia/ffmpeg/ffmpeg_build.sh
@@ -2,8 +2,9 @@
 
 # libuse
 USE_X264=1
+USE_FDK_AAC=1
 
-CLEAN_BEFORE=1
+CLEAN_BEFORE=0
 
 echo "FFmpeg Build Start..."
 FFMPEG_VERSION=4.2.2
@@ -28,16 +29,16 @@ if [ ! -d ${FFMPEG_NAME} ]; then
     tar xvf ${FFMPEG_NAME}.tar.bz2
 fi
 
-export NDK=~/Android/Sdk/ndk/21.3.6528147
-export HOST_TAG=linux-x86_64
-export TOOLCHAINS=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
-export SYSROOT="${TOOLCHAINS}/sysroot"
-export API=23
-export ARCH="aarch64"
-export TARGET=$ARCH-linux-android
-export APP_ABI=arm64-v8a
-export CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-export PREFIX=${PWD}/android-build/
+NDK=~/Android/Sdk/ndk/21.3.6528147
+HOST_TAG=linux-x86_64
+TOOLCHAINS=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
+SYSROOT="${TOOLCHAINS}/sysroot"
+API=23
+ARCH="aarch64"
+CROSS_PREFIX=$ARCH-linux-android
+APP_ABI=arm64-v8a
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+PREFIX=${PWD}/android-build/
 
 echo "TOOLCHAINS: ${TOOLCHAINS}"
 echo "SYSROOT: ${SYSROOT}"
@@ -53,9 +54,9 @@ fi
 build_x264() 
 {
     # wget https://code.videolan.org/videolan/x264/-/archive/master/x264-master.tar.bz2
-    export X264_VERSION=master
-    export X264_NAME=x264-${X264_VERSION}
-    export X264_DOWNLOAD_URL=https://code.videolan.org/videolan/x264/-/archive/master/${X264_NAME}.tar.bz2
+    X264_VERSION=master
+    X264_NAME=x264-${X264_VERSION}
+    X264_DOWNLOAD_URL=https://code.videolan.org/videolan/x264/-/archive/master/${X264_NAME}.tar.bz2
 
     if [ ! -f ${X264_NAME}.tar.bz2 ]; then
         echo "Downloading ${X264_NAME}.tar.bz2"
@@ -68,67 +69,116 @@ build_x264()
     fi
 
     pushd ${X264_NAME}
-    export CC=$TOOLCHAINS/bin/${TARGET}${API}-clang
-    export CXX=$TOOLCHAINS/bin/${TARGET}${API}-clang++
-    export CROSS_PREFIX=$TOOLCHAINS/bin/${TARGET}-
-    echo "CC: ${CC}"
-    echo "CXX: ${CXX}"
-    echo "CROSS_PREFIX: ${CROSS_PREFIX}"
-    ./configure \
+    CC=${TOOLCHAINS}/bin/${CROSS_PREFIX}${API}-clang ./configure \
         --enable-pic \
         --disable-asm \
         --enable-shared \
         --disable-opencl \
         --disable-cli \
         --host=${ARCH}-linux \
-        --cross-prefix=${CROSS_PREFIX} \
+        --cross-prefix=${TOOLCHAINS}/bin/${CROSS_PREFIX}- \
         --sysroot=${SYSROOT} \
         --libdir=${PREFIX}/libs/${APP_ABI} \
         --includedir=${PREFIX}/include/
 
-    if [ "X_${CLEAN_BEFORE}" != "X_" ]; then
+    if [ "X_${CLEAN_BEFORE}" == "X_1" ]; then
         make clean
     fi
     make -j${CORES}
     make install
     rm -rf ${PREFIX}/libs/${APP_ABI}/pkgconfig
-    export X264_SO_PATH=`readlink ${PREFIX}/libs/${APP_ABI}/libx264.so`
+    X264_SO_PATH=`readlink ${PREFIX}/libs/${APP_ABI}/libx264.so`
     rm  ${PREFIX}/libs/${APP_ABI}/libx264.so
     mv ${PREFIX}/libs/${APP_ABI}/${X264_SO_PATH} ${PREFIX}/libs/${APP_ABI}/libx264.so
     rm ${PREFIX}/libs/${APP_ABI}/${X264_SO_PATH}
     popd
 }
 
+build_fdk_aac()
+{
+
+    FDK_AAC_VERSION=2.0.1
+    FDK_AAC_NAME=fdk-aac-${FDK_AAC_VERSION}
+    FDK_AAC_DOWNLOAD_URL=https://downloads.sourceforge.net/opencore-amr/${FDK_AAC_NAME}.tar.gz
+
+    if [ ! -f ${FDK_AAC_NAME}.tar.gz ]; then
+        echo "Downloading ${FDK_AAC_NAME}.tar.gz"
+        download ${FDK_AAC_DOWNLOAD_URL}
+    fi
+
+    if [ ! -d ${FDK_AAC_NAME} ]; then
+        echo "Uncopressing ${FDK_AAC_NAME}.tar.gz ..."
+        tar xvf ${FDK_AAC_NAME}.tar.gz
+    fi
+    
+    pushd ${FDK_AAC_NAME}
+    grep -q "#undef __ANDROID__" libSBRdec/src/lpp_tran.cpp
+    if [ $? != 0 ]; then
+        sed -i "N;120a#undef __ANDROID__" libSBRdec/src/lpp_tran.cpp
+    fi
+
+    CC=${TOOLCHAINS}/bin/${CROSS_PREFIX}${API}-clang \
+    CXX=${TOOLCHAINS}/bin/${CROSS_PREFIX}${API}-clang++ \
+    AR=${TOOLCHAINS}/bin/${CROSS_PREFIX}-ar \
+    AS=${TOOLCHAINS}/bin/${CROSS_PREFIX}-as \
+    LD=${TOOLCHAINS}/bin/${CROSS_PREFIX}-ld \
+    NM=${TOOLCHAINS}/bin/${CROSS_PREFIX}-nm \
+    RANLIB=${TOOLCHAINS}/bin/${CROSS_PREFIX}-ranlib \
+    STRIP=${TOOLCHAINS}/bin/${CROSS_PREFIX}-strip \
+    ./configure \
+        --prefix=${PREFIX} \
+        --enable-shared \
+        --host=aarch64-linux \
+        --with-sysroot=${SYSROOT} \
+        --libdir=${PREFIX}/libs/${APP_ABI} \
+        --includedir=${PREFIX}/include/ \
+        CPPFLAGS="-fPIC"
+
+    if [ "X_${CLEAN_BEFORE}" == "X_1" ]; then
+        make clean
+    fi
+	make -j${CORES}
+	make install
+    rm -rf ${PREFIX}/libs/${APP_ABI}/pkgconfig
+    rm -rf ${PREFIX}/libs/${APP_ABI}/libfdk-aac.a
+    rm -rf ${PREFIX}/libs/${APP_ABI}/libfdk-aac.la
+    FDK_AAC_SO_PATH=`readlink ${PREFIX}/libs/${APP_ABI}/libfdk-aac.so`
+    mv ${PREFIX}/libs/${APP_ABI}/${FDK_AAC_SO_PATH} ${PREFIX}/libs/${APP_ABI}/__${FDK_AAC_SO_PATH} 
+    rm ${PREFIX}/libs/${APP_ABI}/libfdk-aac.so*
+    mv ${PREFIX}/libs/${APP_ABI}/__${FDK_AAC_SO_PATH} ${PREFIX}/libs/${APP_ABI}/libfdk-aac.so
+    popd
+}
+
 build_ffmpeg()
 {
-    if [ "X_${USE_X264}" != "X_" ]; then
-        EXTRA_OPTIONS += "--enable-gpl"
-        EXTRA_OPTIONS += "--enable-libx264"
+    EXTRA_OPTIONS=
+    if [ "X_${USE_X264}" == "X_1" ]; then
+        EXTRA_OPTIONS="${EXTRA_OPTIONS} --enable-gpl"
+        EXTRA_OPTIONS="${EXTRA_OPTIONS} --enable-libx264"
         build_x264
     fi
 
+    if [ "X_${USE_FDK_AAC}" == "X_1" ]; then
+        EXTRA_OPTIONS="${EXTRA_OPTIONS} --enable-nonfree"
+        EXTRA_OPTIONS="${EXTRA_OPTIONS} --enable-libfdk-aac"
+        build_fdk_aac
+    fi
     # FFmpeg build
-    export CONFIG_LOG_PATH=${PREFIX}/log
-    export PWD=`pwd`
-
-    export CC="$TOOLCHAINS/bin/$TARGET$API-clang"
-    export CXX="$TOOLCHAINS/bin/$TARGET$API-clang++"
-    export CROSS_PREFIX="$TOOLCHAINS/bin/$TARGET-"
-    # export LD="${CROSS_PREFIX}ld"
-    export STRIP="${CROSS_PREFIX}strip"
-    export EXTRA_CFLAGS="-D__ANDROID_API__=$API -Os -fPIC -DANDROID"
-    export EXTRA_LDFLAGS="-lc -lm -ldl -llog"
-    export EXTRA_OPTIONS=""
-    echo "CC: ${CC}"
-    echo "CXX: ${CXX}"
-    # echo "LD: ${LD}"
-    echo "CROSS_PREFIX: ${CROSS_PREFIX}"
+    CONFIG_LOG_PATH=${PREFIX}/log
+    PWD=`pwd`
+    EXTRA_CFLAGS="-D__ANDROID_API__=${API} -Os -fPIC -DANDROID -I${PREFIX}/include/"
+    EXTRA_LDFLAGS="-lc -lm -ldl -llog -L${PREFIX}/libs/$APP_ABI"
+    echo "EXTRA_OPTIONS: ${EXTRA_OPTIONS}"
+    echo "cc: ${TOOLCHAINS}/bin/${CROSS_PREFIX}${API}-clang"
     echo "PREFIX: ${PREFIX}"
 
     pushd ${FFMPEG_NAME}
-
     mkdir -p ${CONFIG_LOG_PATH}
     ./configure \
+        --cc=${TOOLCHAINS}/bin/${CROSS_PREFIX}${API}-clang \
+        --cxx=${TOOLCHAINS}/bin/${CROSS_PREFIX}${API}-clang++ \
+        --nm=${TOOLCHAINS}/bin/${CROSS_PREFIX}-nm \
+        --strip=${TOOLCHAINS}/bin/${CROSS_PREFIX}-strip \
         --target-os=android \
         --disable-static \
         --enable-shared \
@@ -166,16 +216,16 @@ build_ffmpeg()
         --incdir=${PREFIX}/include/ \
         --cross-prefix=$CROSS_PREFIX \
         --arch=$ARCH \
-        --sysroot=$SYSROOT \
-        --cc=$CC \
-        --cxx=$CXX \
-        --ld=$LD \
-        --strip=${STRIP} \
+        --sysroot="${SYSROOT}" \
         $EXTRA_OPTIONS \
         --extra-cflags="$EXTRA_CFLAGS" \
         --extra-ldflags="$EXTRA_LDFLAGS"
 
-    if [ "X_${CLEAN_BEFORE}" != "X_" ]; then
+    if [ $? != 0 ]; then
+        echo "ffmpeg configure error."
+        exit 0
+    fi
+    if [ "X_${CLEAN_BEFORE}" == "X_1" ]; then
         make clean
     fi
     make -j${CORES}
